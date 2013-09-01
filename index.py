5eafa62073684978eecd06cd4038d23944f6d3bc
Minras <andrey.shchurkov@gmail.com>
2013-09-01 19:11:03 +0200

    Initial moving of templates to main application

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+# TODO Move debug mode to configuration
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Homepage'
+
+@app.route('/profile')
+def profile():
+    return 'Own profile info'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'Viewing profile of %s' % username
+
+@app.route('/field')
+def index():
+    return render_template('field.html', data='xxx')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
